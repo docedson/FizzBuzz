46f9367d12b08d9fb1effbf4b2c75336c9f304da
Andrew Edson <docedson@gmail.com>
2019-04-11 11:32:33 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numbers = 1; numbers <= 100; numbers++)
+            {
+                //Starting with the most specialized case, and working down to most generic case
+                if (numbers % 3 == 0 && numbers % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (numbers % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+
+                else if (numbers % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(numbers);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
